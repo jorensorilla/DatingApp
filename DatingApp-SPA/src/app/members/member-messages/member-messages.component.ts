59112125c42217202d058941236dea5769ffbbb6
Sorilla <sorillj@global.anz.com>
2019-05-14 11:56:36 +0800

    End of Section 16

--- DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
+++ DatingApp-SPA/src/app/members/member-messages/member-messages.component.ts
@@ -3,6 +3,8 @@ import { Message } from '../../_models/message';
 import { AlertifyService } from '../../_services/alertify.service';
 import { AuthService } from '../../_services/auth.service';
 import { UserService } from '../../_services/user.service';
+import { tap } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-member-messages',
@@ -22,7 +24,17 @@ export class MemberMessagesComponent implements OnInit {
   }
 
   loadMessages() {
+    const currentUserId = +this.authService.decodedToken.nameid;
     this.userService.getMessageThread(this.authService.decodedToken.nameid, this.recipientId)
+      .pipe(
+        tap(messages => {
+          for (let i = 0; i < messages.length; i++) {
+            if (messages[i].isRead === false && this.messages[i].recipientId === currentUserId) {
+              this.userService.markAsRead(currentUserId, messages[i].id);
+            }
+          }
+        })
+      )
       .subscribe(messages => {
         this.messages = messages;
       }, error => {
